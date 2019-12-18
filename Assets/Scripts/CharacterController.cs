68f8436ca7cbb568a5fdd5bda9089ed8aae0c377
Joshua Landron <45753071+jlandron@users.noreply.github.com>
2019-12-18 12:56:25 -0800

    First working version of initial field of view

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+
+    [SerializeField]
+    float moveSPeed = 5f;
+    Rigidbody rigidbody;
+    Camera camera;
+    Vector3 velocity;
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        camera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePos = camera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, camera.transform.position.y));
+        transform.LookAt(mousePos + Vector3.up * transform.position.y);
+        velocity = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized * moveSPeed;
+    }
+
+    private void FixedUpdate()
+    {
+        rigidbody.MovePosition(rigidbody.position + velocity * Time.fixedDeltaTime);
+    }
+}
---
+++ Assets/Scripts/FieldOfView.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldOfView : MonoBehaviour
+{
+    [SerializeField]
+    public float viewRadius;
+    [SerializeField]
+    [Range(0,359)]
+    public float viewAngle;
+
+    public LayerMask targetMask;
+    public LayerMask obsticleMask;
+    public List<Transform> visibleTargets;
+    IEnumerator FindTargetsWithDelay(float delay)
+    {
+        while(true){
+            yield return new WaitForSeconds(delay);
+            FindVisibleTargets();
+        }
+    }
+
+    void FindVisibleTargets()
+    {
+        Collider[] targetsInViewRadius = Physics.OverlapSphere(transform.position, viewRadius, targetMask);
+        visibleTargets.Clear();
+        for (int i = 0; i < targetsInViewRadius.Length; i++)
+        {
+            Transform target = targetsInViewRadius[i].transform;
+            Vector3 directionToTarget = (target.position - transform.position).normalized;
+            //check if its in view angle
+            if (Vector3.Angle(transform.forward, directionToTarget) < viewAngle / 2)
+            {
+                float distanceToTarget = Vector3.Distance(transform.position, target.position);
+                //performa raycast to check for obsticles
+                if(!Physics.Raycast(transform.position, directionToTarget, distanceToTarget, obsticleMask))
+                {
+                    visibleTargets.Add(target);
+                }
+            }
+        }
+    }
+
+
+    public Vector3 DirectionFromAngle(float angleInDegrees, bool angleIsGlobal)
+    {
+        if (!angleIsGlobal)
+        {
+            angleInDegrees += transform.eulerAngles.y;
+        }
+        return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad) , 0, Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
+    }
+
+
+}
